0b65ca10a47273a004d1eca67e5725086dd383e2
EngFatma <eng.fatma.saad1@gmail.com>
2019-10-17 12:01:22 +0200

    first commet

---
+++ assets/js/script.js
@@ -0,0 +1,150 @@
+//loading
+$(document).ready(function() {
+    $('.loading').fadeOut(1000);
+    $('body').css('overflow', 'auto');
+});
+/* wow fire function  */
+new WOW().init();
+
+/* start slick carousel initialization  */
+
+$(document).ready(function() {
+
+    $('.offer_items').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '.before_slide',
+        nextArrow: '.after_slide',
+        responsive: [{
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+
+                }
+            }
+        ]
+
+    });
+    $('.projects').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '.prev_slide',
+        nextArrow: '.next_slide',
+        responsive: [{
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            }
+
+            , {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+
+                }
+            }
+
+        ]
+
+    });
+
+});
+
+/* start slick carousel initialization  */
+
+
+/****** start card carousel  */
+
+
+$num = $('.my-card').length;
+$even = $num / 2;
+$odd = ($num + 1) / 2;
+
+if ($num % 2 == 0) {
+    $('.my-card:nth-child(' + $even + ')').addClass('active');
+    $('.my-card:nth-child(' + $even + ')').prev().addClass('prev');
+    $('.my-card:nth-child(' + $even + ')').next().addClass('next');
+} else {
+    $('.my-card:nth-child(' + $odd + ')').addClass('active');
+    $('.my-card:nth-child(' + $odd + ')').prev().addClass('prev');
+    $('.my-card:nth-child(' + $odd + ')').next().addClass('next');
+}
+
+$('.my-card').click(function() {
+    $slide = $('.active').width();
+    console.log($('.active').position().left);
+
+    if ($(this).hasClass('next')) {
+        $('.card-carousel').stop(false, true).animate({ left: '-=' + $slide });
+    } else if ($(this).hasClass('prev')) {
+        $('.card-carousel').stop(false, true).animate({ left: '+=' + $slide });
+    }
+
+    $(this).removeClass('prev next');
+    $(this).siblings().removeClass('prev active next');
+
+    $(this).addClass('active');
+    $(this).prev().addClass('prev');
+    $(this).next().addClass('next');
+});
+
+
+// Keyboard nav
+$('html body').keydown(function(e) {
+    if (e.keyCode == 37) { // left
+        $('.active').prev().trigger('click');
+    } else if (e.keyCode == 39) { // right
+        $('.active').next().trigger('click');
+    }
+});
+
+//next/prev button
+$('#prev_btn').click(function() {
+    $('.active').prev().trigger('click');
+});
+$('#next-btn').click(function() {
+    $('.active').next().trigger('click');
+});
